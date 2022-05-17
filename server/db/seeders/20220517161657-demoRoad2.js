3678144c475187f6ca00ccb41a2482817e4a54ff
Svetlana <lirica1109@gmail.com>
2022-05-17 19:49:21 +0300

    roads seed

---
+++ server/db/seeders/20220517161657-demoRoad2.js
@@ -0,0 +1,32 @@
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('Roads', [
+      {
+        userId: 1,
+        from: 'Москва',
+        where: 'Санкт-Петербург',
+        mapImg: '/img/mospit.png',
+        discription: 'Путешествие в северную столицу',
+        transportType: 'авто',
+        distance: '712 км',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 1,
+        from: 'Санкт-Петербург',
+        where: 'Кижи',
+        mapImg: '/img/mospit.png',
+        discription: 'Прекрасная Карелия',
+        transportType: 'авто',
+        distance: '438 км',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Roads');
+  },
+};
