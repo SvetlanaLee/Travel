c51f4e922fd3f8e0f4619547cf92417b777eb3b4
Svetlana <lirica1109@gmail.com>
2022-05-24 20:43:40 +0300

    style

--- client/my-app/src/components/UserLogin/UserLogin.jsx
+++ client/my-app/src/components/UserLogin/UserLogin.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+
+
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -66,37 +69,37 @@ export default function UserSignin() {
   }
 
   return (
-    <>
-   
+    <div className='pageLogReg'>
+    {/* <Box></Box> */}
         
      <form className={classes.form} onSubmit={submitHandler}>
      <div  style={{ color: 'red' }}>
      <div>{errorLogin.error}</div>
      </div>
              
-    <TextField 
+     <TextField 
 
-     margin="normal"
+      margin="normal"
       required
       fullWidth
       autoFocus
-       
-     id="outlined-basic1"
-     label="E-mail"
-     variant="outlined"
-     type="email" 
-     name="email"
-     value={inputs.email ?? ''}
-     onChange={(e) => dispatch({ type: 'USER_TYPING', payload: { [e.target.name]: e.target.value } })}/>
-    
-    <TextField 
+        
+      id="outlined-basic1"
+      label="E-mail"
+      variant="outlined"
+      type="email" 
+      name="email"
+      value={inputs.email ?? ''}
+      onChange={(e) => dispatch({ type: 'USER_TYPING', payload: { [e.target.name]: e.target.value } })}/>
+          
+      <TextField 
       margin="normal"
       required
       fullWidth
       autoFocus
       
       id="outlined-basic2"
-      label="Password"
+      label="Пароль"
       variant="outlined"
       type="password"
       name="password"
@@ -112,7 +115,7 @@ export default function UserSignin() {
   
         
     </form>
-    </>
+    </div>
   )
 }
 
--- client/my-app/src/pages/PageRoads/PageRoads.jsx
+++ client/my-app/src/pages/PageRoads/PageRoads.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import Roads from '../../components/Roads/Roads'
-import {Box, TextField, Button} from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Roads from '../../components/Roads/Roads'
+import {Box, TextField, Button} from '@mui/material';
 
 export default function PageRoads() {
 
